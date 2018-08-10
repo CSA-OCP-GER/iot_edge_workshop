a92bf25cc3a05cbfbe74a119cd1debfd186e5219
erjosito <erjosito@hotmail.com>
2018-08-10 16:26:12 +0200

    typo fixed

--- IotEdgeWorkshop/modules/chmodQemu.sh
+++ IotEdgeWorkshop/modules/chmodQemu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chmod 777 /home/jose/myagent/_work/1/s/IotEdgeWorkshop/modules/FilterModule/go-webapp-arm/qemu-arm-static
-chmod 777 /home/jose/myagent/_work/1/s/IotEdgeWorkshop/modules/TempSensor/go-webapp-arm/qemu-arm-static
+chmod 777 /home/jose/myagent/_work/1/s/IotEdgeWorkshop/modules/FilterModule/qemu-arm-static
+chmod 777 /home/jose/myagent/_work/1/s/IotEdgeWorkshop/modules/TempSensor/qemu-arm-static
