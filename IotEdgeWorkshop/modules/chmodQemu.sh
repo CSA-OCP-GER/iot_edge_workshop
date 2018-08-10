d488cebfdfc2515f75996bd8885e4e7dce58e4f0
erjosito <erjosito@hotmail.com>
2018-08-10 16:18:25 +0200

    chmodQemu

---
+++ IotEdgeWorkshop/modules/chmodQemu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod 777 /home/jose/myagent/_work/1/s/IotEdgeWorkshop/modules/FilterModule/go-webapp-arm/qemu-arm-static
+chmod 777 /home/jose/myagent/_work/1/s/IotEdgeWorkshop/modules/TempSensor/go-webapp-arm/qemu-arm-static
