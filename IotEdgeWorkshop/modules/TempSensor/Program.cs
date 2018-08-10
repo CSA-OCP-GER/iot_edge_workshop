38195a8dbb00553a663987a4b2f3efda23c4c9a1
erjosito <erjosito@hotmail.com>
2018-08-10 17:12:39 +0200

    root cert in TempSensor

--- IotEdgeWorkshop/modules/TempSensor/Program.cs
+++ IotEdgeWorkshop/modules/TempSensor/Program.cs
@@ -63,7 +63,8 @@ namespace AzureIotEdgeSimulatedTemperatureSensor
         /// </summary>
         static void InstallCert()
         {
-            var certPath = Environment.GetEnvironmentVariable("EdgeModuleCACertificateFile");
+            //var certPath = Environment.GetEnvironmentVariable("EdgeModuleCACertificateFile");
+            certPath = "/app/azure-iot-test-only.root.ca.cert.pem"
             if (string.IsNullOrWhiteSpace(certPath))
             {
                 // We cannot proceed further without a proper cert file
